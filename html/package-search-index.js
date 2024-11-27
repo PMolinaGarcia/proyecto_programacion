30585f2599407b7a85bc390887d917115d1d0269
PABLO MOLINA GARCÍA <pablo.molinagarcia1@usp.ceu.es>
2024-11-27 22:32:13 +0100

    Adición del javadoc actualizado

--- html/package-search-index.js
+++ html/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"aplicacion"},{"l":"dominio"},{"l":"presentacion"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"aplicacion"},{"l":"dominio"},{"l":"excepciones"},{"l":"presentacion"}];updateSearchResults();
